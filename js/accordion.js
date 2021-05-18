fa96a42d00df8aaeaf1a72db707da65700003ffd
gothamlies <shaneez.osu@gmail.com>
2021-05-18 21:40:30 +0500

    format

--- js/accordion.js
+++ js/accordion.js
@@ -5,10 +5,13 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
     for (let i = 0; i < featureLinkElems.length; i++) {
         featureLinkElems[i].addEventListener('click', () => {
-            if(featureLinkElems[i].classList.contains('feature__link_active')) {
+            if(featureLinkElems[i].classList.contains('feature__link_active')) 
+            {
                 featureLinkElems[i].classList.remove('feature__link_active')
                 featureSubElems[i].classList.add('hidden')
-            } else {
+            } 
+            else 
+            {
                 featureLinkElems.forEach((featureLinkElem) => {
                     featureLinkElem.classList.remove('feature__link_active')
                 })
