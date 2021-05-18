270df972ede4f0e94d3224cc2a14d7ddd8c4731a
gothamlies <shaneez.osu@gmail.com>
2021-05-18 21:01:31 +0500

    first commit

---
+++ js/accordion.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    
+    const featureLinkElems = document.querySelectorAll(".feature__link");
+    const featureSubElems = document.querySelectorAll(".feature-sub");
+
+    for (let i = 0; i < featureLinkElems.length; i++) {
+        featureLinkElems[i].addEventListener('click', () => {
+            if(featureLinkElems[i].classList.contains('feature__link_active')) {
+                featureLinkElems[i].classList.remove('feature__link_active')
+                featureSubElems[i].classList.add('hidden')
+            } else {
+                featureLinkElems.forEach((featureLinkElem) => {
+                    featureLinkElem.classList.remove('feature__link_active')
+                })
+                featureSubElems.forEach((featureSubElem) => {
+                    featureSubElem.classList.add('hidden')
+                })
+                featureLinkElems[i].classList.add('feature__link_active')
+                featureSubElems[i].classList.remove('hidden')
+            }
+        })
+    }
+});
+
